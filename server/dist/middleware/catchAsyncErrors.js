c80deee92de28c6bf26c0c8bb5a7a2af1ce8d419
M TOQEER ZIA <muhammadtoqeerzia586694@gmail.com>
2025-08-08 11:42:20 +0500

    solve a bug2

--- server/dist/middleware/catchAsyncErrors.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.catchAsyncErrors = void 0;
-const catchAsyncErrors = (theFunc) => (req, res, next) => {
-    Promise.resolve(theFunc(req, res, next)).catch(next);
-};
-exports.catchAsyncErrors = catchAsyncErrors;
