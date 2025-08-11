ef0cfff6762207db0b22a0e11103308c219e3ea0
M TOQEER ZIA <muhammadtoqeerzia586694@gmail.com>
2025-08-11 23:52:07 +0500

    response timeout error

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -3,9 +3,14 @@ require("dotenv").config();
 const dbURl: String = process.env.MONOGODB_URI || "";
 const dbConnection = async () => {
   try {
-    await mongoose.connect(`${dbURl}`).then((data: any) => {
-      console.log(`MongoDB connected successfully: ${data.connection.host}`);
+    const connected=await mongoose.connect(`${dbURl}`, {
+      serverSelectionTimeoutMS: 30000,
+      socketTimeoutMS: 45000,
+      retryWrites: true,
+      maxPoolSize: 10,
     });
+    console.log(`MongoDB connected successfully: ${connected.connection.host}`);
+
   } catch (error) {
     console.error("Error", error);
     setTimeout(dbConnection, 5000);
